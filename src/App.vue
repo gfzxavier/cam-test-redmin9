bd07cc7f594b4c231bd830f90494da092d7bfcc3
Gabriel F. Xavier <gfzxavier@outlook.com>
2023-07-26 12:23:37 -0300

    feat: add a camera logic clone

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Visão da câmera
+    <CameraRecordingFm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CameraRecordingFm from './components/CameraRecordingFm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CameraRecordingFm
   }
 }
 </script>
